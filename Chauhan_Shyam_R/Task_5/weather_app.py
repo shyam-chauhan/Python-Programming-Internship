225f58894ddc33c5fbd8cbd31197d7836c382795
shyam chauhan <mitsme44@gmail.com>
2024-07-02 11:51:18 +0530

    Task_6 web scrapper completed

--- Chauhan_Shyam_R/Task_5/weather_app.py
+++ Chauhan_Shyam_R/Task_5/weather_app.py
@@ -1,6 +1,6 @@
 import requests
 
-API_key = '01234567890'
+API_key = 'f4c6b56f806d4b3b2b26221c5805268a'
 
 def get_weather(city):
     url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_key}&units=metric'
